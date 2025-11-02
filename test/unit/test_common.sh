47395806db6fd8cc22c54935290b2f4ed24c7eec
madophs <jehuruvj@gmail.com>
2025-11-02 16:42:12 -0600

    reorder delete of files Just reformating the code

--- test/unit/test_common.sh
+++ test/unit/test_common.sh
@@ -12,13 +12,13 @@ function test_delete_old_files() {
     local -i current_epoch_date=$(date +%s)
     local -i past_epoch_date_61_days=$(( current_epoch_date - (60 * 60 * 24 * 61) ))
     local past_datetime_61_days="$(date -d "@${past_epoch_date_61_days}")"
+
     mkdir -p "${TEST_TEMP_DIR}/files"
     touch -m -d "${past_datetime_61_days}" "${TEST_TEMP_DIR}/files/inside.txt"
     touch -m -d "${past_datetime_61_days}" "${TEST_TEMP_DIR}/files/inside2.txt"
+    touch -m -d "${past_datetime_61_days}" "${TEST_TEMP_DIR}/files"
     touch -m -d "${past_datetime_61_days}" "${TEST_TEMP_DIR}/todelete1.txt"
     touch -m -d "${past_datetime_61_days}" "${TEST_TEMP_DIR}/todelete2.txt"
-    touch -m -d "${past_datetime_61_days}" "${TEST_TEMP_DIR}/files"
-
     touch "${TEST_TEMP_DIR}/nottodelete.txt"
 
     export DAYS_BEFORE_DELETION=60
