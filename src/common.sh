1daaef3df258a5c47d8e4ba8bb88f7ae634089ef
madophs <jehuruvj@gmail.com>
2025-12-29 04:55:54 -0600

    Remove metrics methods

--- src/common.sh
+++ src/common.sh
@@ -356,25 +356,6 @@ function common_setup() {
     delete_old_files ${BUILD_DIR}
 }
 
-clock_start() {
-   [[ ! -v __CALLS_COUNTER ]] && declare -g -i __CALLS_COUNTER=0 # Clock counter used as varname's suffix
-   [[ ! -v __CLOCK_STACK ]] && declare -g -a __CLOCK_STACK=() # Stack containing internal clocks
-   declare -g -i __clock_start_${__CALLS_COUNTER}=$(( $(date +%s%N) / 1000 )) # Start time in microseconds
-   __CLOCK_STACK+=( __clock_start_${__CALLS_COUNTER} )
-   __CALLS_COUNTER+=1
-}
-
-# @brief computes total time spend in a function
-# the result can be found in __total_spend_time in microseconds
-clock_end() {
-    local -i __clock_end=$(( $(date +%s%N) / 1000 ))
-    local __clock_start_var=${__CLOCK_STACK[-1]}
-    __total_spend_time=$( echo "(${__clock_end} - ${!__clock_start_var}) / 1000000" | bc -l )
-    printf -v __total_spend_time "%.6f" ${__total_spend_time}
-    cout debug "Total time spend at <${YELLOW}${FUNCNAME[1]}${BLK}> is ${CYAN}${__total_spend_time}${BLK} seconds."
-    unset __CLOCK_STACK[-1] # Pop last variable time as will not be used again
-}
-
 function display_help() {
     printf "Usage: mdscode [options] file...\n"
     printf "Options:\n"
